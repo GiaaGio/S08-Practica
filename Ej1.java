b27c424bfc3688cbaf75eef1e9bbba4f9097ffe5
GiaaGio <166878344+GiaaGio@users.noreply.github.com>
2024-05-27 11:39:46 -0500

    Add files via upload

---
+++ Ej1.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Ej1 {
+
+    public static void main(String[] args) {
+        Ej1 bienvenido = new Ej1();
+        bienvenido.saludar();
+    }
+
+    public void saludar() {
+        System.out.println("Hola Mudo");
+    }
+}
